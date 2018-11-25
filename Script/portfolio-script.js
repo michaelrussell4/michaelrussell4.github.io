57b4d7b74900e46f0723b9516593c3d1e39d9be0
Michael Russell <michael.russell.ut@gmail.com>
2018-11-24 19:01:12 -0700

    Update from 11/24/2018; added projects and contact cards

--- Script/portfolio-script.js
+++ Script/portfolio-script.js
@@ -12,6 +12,29 @@ function loadBackgroundText() {
 						  'Custom ', 
 						  'Style ',
 						  'Modern ']; 
-	for(i=0; i<100; i++)
+	for(i=0; i<200; i++)
 		document.getElementById("background-text").innerHTML += backgroundAttr[i%backgroundAttr.length]; 
+}
+
+function pullDownCard(id) {
+	var x = document.getElementById(id); 
+	var y = document.getElementById("me-btn"); 
+	if(x.className.indexOf("w3-show") == -1) {
+		x.className += " w3-show"; 
+		y.innerHTML = "&#9650;";
+	} 
+	else {
+		x.className = x.className.replace(" w3-show", ""); 
+		y.innerHTML = "&#9660;";
+	}
+}
+
+function accordianPullDown(id) {
+	var x = document.getElementById(id); 
+	if(x.className.indexOf("w3-show") == -1) {
+		x.className += " w3-show"; 
+	} 
+	else {
+		x.className = x.className.replace(" w3-show", ""); 
+	}
 }
