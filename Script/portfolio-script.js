2fc274348c81c5da092e34f6374329e9c6b62129
Michael Russell <37958894+michaelrussell4@users.noreply.github.com>
2019-01-07 17:32:26 -0700

    Made fibonacci code

--- Script/portfolio-script.js
+++ Script/portfolio-script.js
@@ -40,3 +40,34 @@ function turingTest() {
 function reverseString(str) {
 	return str.split('').reverse().join('');
 }
+
+function fibonnaci() {
+	var valIn = document.getElementById('fib-input').value;
+	var valOut = document.getElementById('fib-output');
+	var output = false; 
+	var pattern = /^[1-9]{1}[0-9]*/i;
+	var inLang = pattern.test(valIn);
+	if(!inLang){
+		valOut.style.color = "red";
+		valOut.value = "Invalid input. Enter a number.";
+		return;
+	}
+	if(valIn.length > 4){
+		valOut.style.color = "red";
+		valOut.value = "That number's too big...";
+		return;
+	}
+	valOut.style.color = "black";
+	valOut.value = fib(valIn);
+}
+
+function fib(x) {
+	if(x==0) return x;
+	var p = 0, q = 1, r;
+	for(var i=0; i<x; i++){
+		r = p + q;
+		p = q; 
+		q = r; 
+	}
+	return q;
+}
