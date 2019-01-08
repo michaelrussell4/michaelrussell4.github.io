ecbd7bbac2cfc5ad224dae5e9f9958f114fdb1ab
Michael Russell <37958894+michaelrussell4@users.noreply.github.com>
2019-01-07 18:54:58 -0700

    fixed java files

--- Resources/TM3.java
+++ Resources/TM3.java
@@ -7,39 +7,39 @@
 
 public class TM3 
 { 
-  public static void main(String[] args) 
-  { 
-  	String[] C = 
-    	{"0,x=>#,R,1", 
-    	"1,x=>x,R,1", 
-    	"1,y=>y,R,1", 
-    	"1,#=>#,L,2", 
-    	"2,#=>#,R,7", 
-    	"2,x=>#,L,3", 
-    	"3,x=>x,L,3", 
-    	"3,y=>y,L,3", 
-    	"3,#=>#,R,0", 
-    	"3,B=>B,R,0", 
-    	"0,y=>#,R,4", 
-    	"4,y=>y,R,4", 
-    	"4,x=>x,R,4", 
-    	"4,#=>#,L,5", 
-    	"5,#=>#,R,7", 
-    	"5,y=>#,L,6", 
-    	"6,y=>y,L,6", 
-    	"6,x=>x,L,6", 
-    	"6,#=>#,R,0"}; 
-  	char[] ST = {'R','R','R','R','R','R','R','H'}; 
-   
-  	String inString; 
-  	boolean accept1; 
-  	TM TM1 = new TM(C, ST); 
-  	if(args.length >= 1){ 
-   	// Input string is command line parameter 
-   	inString = args[0]; 
-   	// Process input string 
-   	accept1 = TM1.processData(inString); 
-   	System.out.println(inString+"\t\t Valid string? " + accept1); 
-  	} 
-  }// end main 
+    public static void main(String[] args) 
+    { 
+        String[] C = 
+            {"0,x=>#,R,1", 
+            "1,x=>x,R,1", 
+            "1,y=>y,R,1", 
+            "1,#=>#,L,2", 
+            "2,#=>#,R,7", 
+            "2,x=>#,L,3", 
+            "3,x=>x,L,3", 
+            "3,y=>y,L,3", 
+            "3,#=>#,R,0", 
+            "3,B=>B,R,0", 
+            "0,y=>#,R,4", 
+            "4,y=>y,R,4", 
+            "4,x=>x,R,4", 
+            "4,#=>#,L,5", 
+            "5,#=>#,R,7", 
+            "5,y=>#,L,6", 
+            "6,y=>y,L,6", 
+            "6,x=>x,L,6", 
+            "6,#=>#,R,0"}; 
+        char[] ST = {'R','R','R','R','R','R','R','H'}; 
+        
+        String inString; 
+        boolean accept1; 
+        TM TM1 = new TM(C, ST); 
+        if(args.length >= 1){ 
+            // Input string is command line parameter 
+            inString = args[0]; 
+            // Process input string 
+            accept1 = TM1.processData(inString); 
+            System.out.println(inString+"\t\t Valid string? " + accept1); 
+        } 
+    }// end main 
 } // end class
